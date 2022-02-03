0b5102b34fc2d5f47ec1b436f291ba646ad68894
Earnox <stevenson74@hotmail.fr>
2022-02-03 21:34:25 +0100

    add color rule and class auto generated

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const GetData = () => {
   const url =
-    "https://script.google.com/macros/s/AKfycbzJDVPoFJlGrtbGO_fbequfLBIxDg00A4clTYSHwdw0-TxZd54Uf--v89utV2BbDnpN5Q/exec";
+    "https://script.google.com/macros/s/AKfycbyAD4E6D4mX3sMR_sC23nmSwrR7AMNKEOLF2PH-azPdSWS3Ma5cAuvtTkTdbDwlZxN2Gg/exec";
   fetch(url).then((reponses) =>
     reponses.json().then((responses) => {
       const divTableauApp = document.getElementById("tableauNumeroapp");
       let table = document.createElement("table");
       let headerrow = document.createElement("tr");
       let headers = Object.keys(responses[0]);
+      delete headers[6];
+      let bagroundColorColum = [];
+      //console.log(Object.keys(responses[0]));
 
-      console.log(Object.keys(responses[0]));
       headers.forEach((headertext) => {
         let textNodeHeader = document.createTextNode(headertext);
         let header = document.createElement("th");
@@ -19,9 +21,50 @@ const GetData = () => {
       responses.forEach((response) => {
         let row = document.createElement("tr");
         row.id = "appt" + response.numeroApp;
+        bagroundColorColum.push(response.colorBagroud);
+        // console.log(bagroundColorColum);
+        delete response.colorBagroud;
+
         Object.values(response).forEach((text) => {
+          // console.log(response);
           let cell = document.createElement("td");
           let textnode = document.createTextNode(text);
+          console.log("ici log text    " + text);
+          console.log("ici rps status    " + response.status);
+          // get if text is numb app then put the bagroud color
+          if (text == response.numeroApp) {
+            cell.classList = "t" + response.typologie;
+          }
+          if (text == response.status) {
+            // switch (text) {
+            //   case text == "occupé":
+            //     cell.classList = "occuper";
+            //     break;
+            //   case text == "app en chauffe":
+            //     cell.classList = "app-en-chauffe";
+            //     break;
+            //   case text == "prêt":
+            //     cell.classList = "pret";
+            //     break;
+            //   case text == "prêt BCS":
+            //     cell.classList = "pret-bcs";
+
+            //   case text == "libre/sale":
+            //     cell.classList = "libre-sale";
+            // }
+            if (text == "occupé") {
+              cell.classList = "occuper";
+            } else if (text == "app en chauffe") {
+              cell.classList = "app-en-chauffe";
+            } else if (text == "prêt") {
+              cell.classList = "pret";
+            } else if (text == "prêt BCS") {
+              cell.classList = "pret-bcs";
+            } else if (text == "libre/sale") {
+              cell.classList = "libre-sale";
+            }
+          }
+
           cell.appendChild(textnode);
           row.appendChild(cell);
         });
